52973da950b77526988c4062ca332fa4abddef46
Chase Gibbons <chase@rentdynamics.com>
2018-03-19 20:43:44 -0600

    refactor(removed material2 stuff): from documentation-items.ts
    to avoid copyright issues

--- src/app/shared/documentation-items/documentation-items.ts
+++ src/app/shared/documentation-items/documentation-items.ts
@@ -18,7 +18,7 @@ const CDK = 'cdk';
 const COMPONENTS = 'components';
 export const SECTIONS = {
   [COMPONENTS]: 'Components',
-  [CDK]: 'CDK',
+  // [CDK]: 'CDK',
 };
 
 
@@ -38,156 +38,156 @@ const DOCS: {[key: string]: DocCategory[]} = {
         {id: 'blocked-appt-time', name: 'Overview', examples: ['blocked-appt-time-overview']},
       ]
     },
-    {
-      id: 'forms',
-      name: 'Form Controls',
-      items: [
-        {id: 'autocomplete', name: 'Autocomplete', examples: ['autocomplete-overview']},
-        {id: 'checkbox', name: 'Checkbox', examples: ['checkbox-configurable']},
-        {
-          id: 'datepicker',
-          name: 'Datepicker',
-          examples: [
-            'datepicker-overview',
-            'datepicker-start-view',
-            'datepicker-value',
-            'datepicker-min-max',
-            'datepicker-filter',
-            'datepicker-events',
-            'datepicker-disabled',
-            'datepicker-touch',
-            'datepicker-api',
-            'datepicker-locale',
-            'datepicker-moment',
-            'datepicker-formats',
+    // {
+    //   id: 'forms',
+    //   name: 'Form Controls',
+    //   items: [
+    //     {id: 'autocomplete', name: 'Autocomplete', examples: ['autocomplete-overview']},
+    //     {id: 'checkbox', name: 'Checkbox', examples: ['checkbox-configurable']},
+    //     {
+    //       id: 'datepicker',
+    //       name: 'Datepicker',
+    //       examples: [
+    //         'datepicker-overview',
+    //         'datepicker-start-view',
+    //         'datepicker-value',
+    //         'datepicker-min-max',
+    //         'datepicker-filter',
+    //         'datepicker-events',
+    //         'datepicker-disabled',
+    //         'datepicker-touch',
+    //         'datepicker-api',
+    //         'datepicker-locale',
+    //         'datepicker-moment',
+    //         'datepicker-formats',
 
-          ]
-        },
-        {
-          id: 'form-field',
-          name: 'Form field',
-          examples: [
-            'form-field-overview',
-            'form-field-label',
-            'form-field-hint',
-            'form-field-error',
-            'form-field-prefix-suffix',
-            'form-field-theming',
-            'form-field-custom-control',
-          ]
-        },
-        {
-          id: 'input',
-          name: 'Input',
-          examples: [
-            'input-overview',
-            'input-error-state-matcher',
-            'input-autosize-textarea',
-            'input-clearable',
-            'input-errors',
-            'input-form',
-            'input-hint',
-            'input-prefix-suffix',
-          ]
-        },
-        {id: 'radio', name: 'Radio button', examples: ['radio-ng-model']},
-        {
-          id: 'select',
-          name: 'Select',
-          examples: [
-            'select-overview',
-            'select-value-binding',
-            'select-form',
-            'select-hint-error',
-            'select-disabled',
-            'select-reset',
-            'select-optgroup',
-            'select-multiple',
-            'select-custom-trigger',
-            'select-no-ripple',
-            'select-panel-class',
-            'select-error-state-matcher',
-          ]
-        },
-        {id: 'slider', name: 'Slider', examples: ['slider-configurable']},
-        {id: 'slide-toggle', name: 'Slide toggle', examples: ['slide-toggle-configurable']},
-     ]
-    },
-    {
-      id: 'nav',
-      name: 'Navigation',
-      summary: 'Sidenavs, toolbars, menus',
-      items: [
-        {id: 'menu', name: 'Menu', examples: ['menu-icons']},
-        {
-          id: 'sidenav',
-          name: 'Sidenav',
-          examples: [
-            'sidenav-overview',
-            'sidenav-drawer-overview',
-            'sidenav-position',
-            'sidenav-open-close',
-            'sidenav-mode',
-            'sidenav-disable-close',
-            'sidenav-autosize',
-            'sidenav-fixed',
-            'sidenav-responsive'
-          ]
-        },
-        {id: 'toolbar', name: 'Toolbar', examples: ['toolbar-multirow']},
-      ]
-    },
-    {
-      id: 'layout',
-      name: 'Layout',
-      items: [
-        {id: 'card', name: 'Card', examples: ['card-fancy']},
-        {id: 'divider', name: 'Divider', examples: ['divider-overview']},
-        {id: 'expansion', name: 'Expansion Panel',
-            examples: ['expansion-overview', 'expansion-steps']},
-        {id: 'grid-list', name: 'Grid list', examples: ['grid-list-dynamic']},
-        {id: 'list', name: 'List', examples: ['list-sections']},
-        {id: 'stepper', name: 'Stepper', examples: ['stepper-overview']},
-        {id: 'tabs', name: 'Tabs', examples: ['tabs-template-label']},
-      ]
-    },
-    {
-      id: 'buttons',
-      name: 'Buttons & Indicators',
-      items: [
-        {id: 'button', name: 'Button', examples: ['button-types']},
-        {id: 'button-toggle', name: 'Button toggle', examples: ['button-toggle-exclusive']},
-        {id: 'chips', name: 'Chips', examples: ['chips-stacked']},
-        {id: 'icon', name: 'Icon', examples: ['icon-svg']},
-        {id: 'progress-spinner', name: 'Progress spinner',
-            examples: ['progress-spinner-configurable']},
-        {id: 'progress-bar', name: 'Progress bar', examples: ['progress-bar-configurable']},
-      ]
-    },
-    {
-      id: 'modals',
-      name: 'Popups & Modals',
-      items: [
-        {id: 'dialog', name: 'Dialog', examples: ['dialog-overview']},
-        {id: 'snack-bar', name: 'Snackbar', examples: ['snack-bar-component']},
-        {id: 'tooltip', name: 'Tooltip', examples: ['tooltip-position']},
-      ]
-    },
-    {
-      id: 'tables',
-      name: 'Data table',
-      items: [
-        {id: 'paginator', name: 'Paginator', examples: ['paginator-configurable']},
-        {id: 'sort', name: 'Sort header', examples: ['sort-overview']},
-        {id: 'table', name: 'Table', examples: [
-          'table-filtering',
-          'table-pagination',
-          'table-sorting',
-          'table-http',
-          'table-overview',
-        ]},
-      ]
-    }
+    //       ]
+    //     },
+    //     {
+    //       id: 'form-field',
+    //       name: 'Form field',
+    //       examples: [
+    //         'form-field-overview',
+    //         'form-field-label',
+    //         'form-field-hint',
+    //         'form-field-error',
+    //         'form-field-prefix-suffix',
+    //         'form-field-theming',
+    //         'form-field-custom-control',
+    //       ]
+    //     },
+    //     {
+    //       id: 'input',
+    //       name: 'Input',
+    //       examples: [
+    //         'input-overview',
+    //         'input-error-state-matcher',
+    //         'input-autosize-textarea',
+    //         'input-clearable',
+    //         'input-errors',
+    //         'input-form',
+    //         'input-hint',
+    //         'input-prefix-suffix',
+    //       ]
+    //     },
+    //     {id: 'radio', name: 'Radio button', examples: ['radio-ng-model']},
+    //     {
+    //       id: 'select',
+    //       name: 'Select',
+    //       examples: [
+    //         'select-overview',
+    //         'select-value-binding',
+    //         'select-form',
+    //         'select-hint-error',
+    //         'select-disabled',
+    //         'select-reset',
+    //         'select-optgroup',
+    //         'select-multiple',
+    //         'select-custom-trigger',
+    //         'select-no-ripple',
+    //         'select-panel-class',
+    //         'select-error-state-matcher',
+    //       ]
+    //     },
+    //     {id: 'slider', name: 'Slider', examples: ['slider-configurable']},
+    //     {id: 'slide-toggle', name: 'Slide toggle', examples: ['slide-toggle-configurable']},
+    //  ]
+    // },
+    // {
+    //   id: 'nav',
+    //   name: 'Navigation',
+    //   summary: 'Sidenavs, toolbars, menus',
+    //   items: [
+    //     {id: 'menu', name: 'Menu', examples: ['menu-icons']},
+    //     {
+    //       id: 'sidenav',
+    //       name: 'Sidenav',
+    //       examples: [
+    //         'sidenav-overview',
+    //         'sidenav-drawer-overview',
+    //         'sidenav-position',
+    //         'sidenav-open-close',
+    //         'sidenav-mode',
+    //         'sidenav-disable-close',
+    //         'sidenav-autosize',
+    //         'sidenav-fixed',
+    //         'sidenav-responsive'
+    //       ]
+    //     },
+    //     {id: 'toolbar', name: 'Toolbar', examples: ['toolbar-multirow']},
+    //   ]
+    // },
+    // {
+    //   id: 'layout',
+    //   name: 'Layout',
+    //   items: [
+    //     {id: 'card', name: 'Card', examples: ['card-fancy']},
+    //     {id: 'divider', name: 'Divider', examples: ['divider-overview']},
+    //     {id: 'expansion', name: 'Expansion Panel',
+    //         examples: ['expansion-overview', 'expansion-steps']},
+    //     {id: 'grid-list', name: 'Grid list', examples: ['grid-list-dynamic']},
+    //     {id: 'list', name: 'List', examples: ['list-sections']},
+    //     {id: 'stepper', name: 'Stepper', examples: ['stepper-overview']},
+    //     {id: 'tabs', name: 'Tabs', examples: ['tabs-template-label']},
+    //   ]
+    // },
+    // {
+    //   id: 'buttons',
+    //   name: 'Buttons & Indicators',
+    //   items: [
+    //     {id: 'button', name: 'Button', examples: ['button-types']},
+    //     {id: 'button-toggle', name: 'Button toggle', examples: ['button-toggle-exclusive']},
+    //     {id: 'chips', name: 'Chips', examples: ['chips-stacked']},
+    //     {id: 'icon', name: 'Icon', examples: ['icon-svg']},
+    //     {id: 'progress-spinner', name: 'Progress spinner',
+    //         examples: ['progress-spinner-configurable']},
+    //     {id: 'progress-bar', name: 'Progress bar', examples: ['progress-bar-configurable']},
+    //   ]
+    // },
+    // {
+    //   id: 'modals',
+    //   name: 'Popups & Modals',
+    //   items: [
+    //     {id: 'dialog', name: 'Dialog', examples: ['dialog-overview']},
+    //     {id: 'snack-bar', name: 'Snackbar', examples: ['snack-bar-component']},
+    //     {id: 'tooltip', name: 'Tooltip', examples: ['tooltip-position']},
+    //   ]
+    // },
+    // {
+    //   id: 'tables',
+    //   name: 'Data table',
+    //   items: [
+    //     {id: 'paginator', name: 'Paginator', examples: ['paginator-configurable']},
+    //     {id: 'sort', name: 'Sort header', examples: ['sort-overview']},
+    //     {id: 'table', name: 'Table', examples: [
+    //       'table-filtering',
+    //       'table-pagination',
+    //       'table-sorting',
+    //       'table-http',
+    //       'table-overview',
+    //     ]},
+    //   ]
+    // }
   ],
   [CDK] : [
     {
